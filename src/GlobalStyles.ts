dbe05eb28ecc669a962cd0b34447411ba21d466c
Arthur Geisweiller <geisweiller@gmail.com>
2021-11-27 18:04:05 -0300

    feat: basic config

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import { theme } from './constants/theme';
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: ${theme.fonts.primary}; 
+};
+
+html, body, #root {
+  height: 100%;
+}
+#root {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.colors.white};
+}
+`;
